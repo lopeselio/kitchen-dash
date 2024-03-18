b8736e637012aa66f4cbe64d160980cb35de3f74
lopeselio <lopeselio@gmail.com>
2024-03-19 00:28:49 +0530

    testing

---
+++ Assets/Testing.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform == transform) {
+            Update();
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    
+                }
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
