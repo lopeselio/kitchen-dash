f2a6188812cb1908f90119f2a6142edd97cbf2a9
lopeselio <lopeselio@gmail.com>
2024-03-26 19:21:33 +0530

    added x and z movement with capsulecast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,33 @@ public class Player : MonoBehaviour
         float playerRadius = .7f;
         float playerHeight = 2f;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove) {
+            // cannot move towards moveDir
+
+            // Attempt movement only in X direction 
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove) {
+                // can move only on the X
+                moveDir = moveDirX;
+            } else {
+                // cannot move on X
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                // Attempt Z only movement
+                if (canMove) {
+                    moveDir = moveDirZ;
+                } else {
+                    // cannot move in any direction
+                }
+
+            }
+
+        }
+
         if (canMove) {
             transform.position += moveDir * moveSpeed * Time.deltaTime;
         }
