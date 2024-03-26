9bfce63f052ba714131acc24b3cbc4805c677a77
lopeselio <lopeselio@gmail.com>
2024-03-26 19:22:28 +0530

    added normalisation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,7 +22,7 @@ public class Player : MonoBehaviour
             // cannot move towards moveDir
 
             // Attempt movement only in X direction 
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if(canMove) {
@@ -30,7 +30,7 @@ public class Player : MonoBehaviour
                 moveDir = moveDirX;
             } else {
                 // cannot move on X
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 // Attempt Z only movement
