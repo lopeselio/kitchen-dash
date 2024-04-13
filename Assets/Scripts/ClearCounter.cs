5512dad7f74fb9a6269b39a1eabc40a4d775b2f4
devELIOper <lopeselio@gmail.com>
2024-04-13 16:44:41 +0530

    interact clear counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    // interact function
     public void Interact() {
         Debug.Log("Player Interacted with ClearCounter");
     }
