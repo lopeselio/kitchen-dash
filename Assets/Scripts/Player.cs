0ae63d1c5676f76e388332ab30e4823b9eca9fdd
devELIOper <lopeselio@gmail.com>
2024-04-16 14:03:53 +0530

    setting player instance: in progress

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask countersLayerMask;
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
     
     
     private void Start() {
@@ -18,20 +24,8 @@ public class Player : MonoBehaviour
     }
 
     private void GameInput_OnInteraction(object sender, System.EventArgs e){
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        if (moveDir != Vector3.zero) {
-            lastInteractDir = moveDir;
-        }
-        float interactDistance = 2f;
-        RaycastHit raycastHit;
-        if (Physics.Raycast(transform.position, lastInteractDir, out raycastHit, interactDistance, countersLayerMask)) {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
-                // Has ClearCounter
-                clearCounter.Interact();
-            }
-        } else {
-            Debug.Log("-");
+        if (selectedCounter != null) {
+            selectedCounter.Interact();
         }
     }
 
@@ -53,10 +47,15 @@ public class Player : MonoBehaviour
         if (Physics.Raycast(transform.position, lastInteractDir, out raycastHit, interactDistance, countersLayerMask)) {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)){
                 // Has ClearCounter
-                // clearCounter.Interact();
+                if (clearCounter != selectedCounter) {
+                    SetSelectedCounter(clearCounter);
+                }
+            } else {
+                SetSelectedCounter(null);
+
             }
         } else {
-            Debug.Log("-");
+            SetSelectedCounter(null);  
         }
     }
 
@@ -109,5 +108,13 @@ public class Player : MonoBehaviour
 
 
         // Debug.Log(Time.deltaTime);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter) {
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+            selectedCounter = selectedCounter;
+        });
     }
 }
