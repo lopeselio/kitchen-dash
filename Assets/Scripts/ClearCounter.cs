e1e1269a2df187fa00d46c9e20e1913617a1be5b
devELIOper <lopeselio@gmail.com>
2024-04-16 16:50:36 +0530

    spawn object upon interact

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
+
+
+
     // interact function
     public void Interact() {
         Debug.Log("Player Interacted with ClearCounter");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 
 }
